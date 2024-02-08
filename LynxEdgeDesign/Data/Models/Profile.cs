fcfdbbdd111f8d593f08f26813381f4c504a838c
AbdelRahmanOdeh <AbdelRahmanOdeh@A-ODEH>
2024-02-08 16:47:04 +0300

    Add project files.

---
+++ LynxEdgeDesign/Data/Models/Profile.cs
@@ -0,0 +1,10 @@
+﻿namespace LynxEdgeDesign.Data.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Name {get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Class { get; set; }
+    }
+}
